9602a74e39dad54c56ee84927d63c976df1d9482
Oliver Forral <intrepion@gmail.com>
2026-04-18 20:54:28 -0600

    Define prism-face-selector Flutter contracts

--- workspace/lib/contracts/normalized_rect.dart
+++ workspace/lib/contracts/normalized_rect.dart
@@ -1,1 +1,13 @@
+class NormalizedRect {
+  final double left;
+  final double top;
+  final double width;
+  final double height;
 
+  const NormalizedRect({
+    required this.left,
+    required this.top,
+    required this.width,
+    required this.height,
+  });
+}
--- workspace/lib/contracts/prism_face_selection.dart
+++ workspace/lib/contracts/prism_face_selection.dart
@@ -1,1 +1,8 @@
+import 'normalized_rect.dart';
 
+class PrismFaceSelection {
+  final String faceName;
+  final NormalizedRect? rect;
+
+  const PrismFaceSelection({required this.faceName, required this.rect});
+}
