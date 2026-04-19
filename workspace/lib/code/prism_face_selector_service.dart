578a014c28a4cf5e3bc8f3bd8cf04310dc46fbf6
Oliver Forral <intrepion@gmail.com>
2026-04-18 20:59:28 -0600

    2. Green: Return The Canonical Empty Face Selection Map

--- workspace/lib/code/prism_face_selector_service.dart
+++ workspace/lib/code/prism_face_selector_service.dart
@@ -1,1 +1,7 @@
+import '../contracts/normalized_rect.dart';
 
+const canonicalPrismFaces = ['front', 'back', 'left', 'right', 'top', 'bottom'];
+
+Map<String, NormalizedRect?> defaultFaceSelectionMap() {
+  return {for (final face in canonicalPrismFaces) face: null};
+}
