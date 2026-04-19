ffdccc664d1b503610289d22c1c95fe37ee8f4da
Oliver Forral <intrepion@gmail.com>
2026-04-18 21:01:08 -0600

    4. Green: Normalize A Selection Rectangle

--- workspace/lib/code/prism_face_selector_service.dart
+++ workspace/lib/code/prism_face_selector_service.dart
@@ -5,3 +5,19 @@ const canonicalPrismFaces = ['front', 'back', 'left', 'right', 'top', 'bottom'];
 Map<String, NormalizedRect?> defaultFaceSelectionMap() {
   return {for (final face in canonicalPrismFaces) face: null};
 }
+
+NormalizedRect normalizeSelectionRect({
+  required double imageWidth,
+  required double imageHeight,
+  required double left,
+  required double top,
+  required double width,
+  required double height,
+}) {
+  return NormalizedRect(
+    left: left / imageWidth,
+    top: top / imageHeight,
+    width: width / imageWidth,
+    height: height / imageHeight,
+  );
+}
