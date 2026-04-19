c5b4ac5724d460f8b8b0d40e4a20905db0a23e5a
Oliver Forral <intrepion@gmail.com>
2026-04-18 20:57:27 -0600

    1. Red: Build The Empty Face Selection Map

--- workspace/test/code/prism_face_selector_service_test.dart
+++ workspace/test/code/prism_face_selector_service_test.dart
@@ -1,1 +1,18 @@
+import 'package:prism_face_selector/code/prism_face_selector_service.dart';
+import 'package:test/test.dart';
 
+void main() {
+  test('defaultFaceSelectionMap returns all empty canonical face slots', () {
+    final selections = defaultFaceSelectionMap();
+
+    expect(selections.keys, [
+      'front',
+      'back',
+      'left',
+      'right',
+      'top',
+      'bottom',
+    ]);
+    expect(selections.values.every((rect) => rect == null), isTrue);
+  });
+}
